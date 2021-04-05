5c6a77f806c52502dbbe46c791b11fe50ed698ed
Zak Stamps <theoneandonlyzako@gmail.com>
2021-04-05 12:34:59 -0700

    fixed browser default refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 // assigns the button element object representation to a variable in the file 
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // dynamically creates the task item
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+  
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  }
+  };
 
 // adds the DOM object reference to the task list
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
